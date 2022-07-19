9f392eb37ff55cb5e78f0b8b691d3c49acd3ad00
Lucas Bonfanti <lucasbonfanti97@gmail.com>
2022-07-19 17:20:43 -0300

    Exercício a007-variáveis concluído

---
+++ exercicio2/script.js
@@ -0,0 +1,24 @@
+// let gostaMusica = ""
+// let tocaInstrumento = ""
+// let pensaAprender = ""
+
+// prompt('Você gosta de música?');
+// prompt('Você toca algum instrumento?');
+// prompt('Já pensou em aprender algum instrumento?');
+
+// gostaMusica = prompt('Você gosta de música?');
+// tocaInstrumento = prompt('Você toca algum instrumento?');
+// pensaAprender = prompt('Já pensou em aprender algum instrumento?');
+
+// console.log(gostaMusica, tocaInstrumento, pensaAprender);
+
+const gostaMusica = "Você gosta de música?"
+const respGostaMusica = prompt(gostaMusica)
+
+
+console.log(gostaMusica, respGostaMusica);
+
+
+
+
+
